22483b22d81bc1e37b4ff8f14db8d0cfadc1223f
berke <bab26@st-andrews.ac.uk>
2022-04-24 02:21:22 +0300

    Added send button disabling before a certain length logic

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -4,8 +4,21 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
     const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('');
 
     const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) : void => {
+        if(text === '') {
+            setBtnDisabled(true);
+            setMessage('');
+        } else if(text !== '' && text.length <= 10) {
+            setBtnDisabled(true);
+            setMessage('Message should at least be 10 characters');
+        } else {
+            setMessage('');
+            setBtnDisabled(false);
+        }
+
         setText(e.target.value);
     }
 
@@ -16,8 +29,10 @@ function FeedbackForm() {
             {/* @todo - rating select component */}
             <div className="input-group">
                 <input type="text" onChange={handleTextChange} placeholder="Write a review..." value={text}/>
-                <Button type="submit">Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
