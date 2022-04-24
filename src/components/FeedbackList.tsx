bfaa3c9589007c1663fe5b1f5e53b17672bda521
berke <bab26@st-andrews.ac.uk>
2022-04-24 04:33:22 +0300

    Added simple animations to feedbacks

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem';
+import {motion, AnimatePresence} from 'framer-motion';
 
 function FeedbackList({ feedback, handleDelete } : IFeedbackListProps) {
   if(!feedback || feedback.length === 0) {
@@ -8,9 +9,18 @@ function FeedbackList({ feedback, handleDelete } : IFeedbackListProps) {
   
     return (
     <div className="feedback-list">
+      <AnimatePresence>
         {feedback.map((item) => (
+          <motion.div 
+            key={item.id}
+            initial={{ opacity:0 }}
+            animate={{ opacity:1 }}
+            exit={{ opacity:0 }} 
+          >
             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+          </motion.div> 
         ))}
+      </AnimatePresence>
     </div>
   )
 }
